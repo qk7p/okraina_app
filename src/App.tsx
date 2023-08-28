53bd8f5f9908327880d6ab2cd9048b779c4c355e
qk7p <1nssine@gmail.com>
2023-08-28 20:38:22 +0300

    фикс

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ProductContainer } from "./components/ProductContainer/ProductContainer
 
 
 function App() {
-  const [activeTab, setActiveTab] = useState<number>(1);
+  const [activeTab, setActiveTab] = useState<number>(5);
 
   const handleTab = (tabId: number) => {
     setActiveTab(tabId);
